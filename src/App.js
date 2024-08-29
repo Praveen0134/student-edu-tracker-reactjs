0390c95f1ffd3fd4ed50dd285c89accae5115e71
Praveen kumar <praveen132004jaya@gmail.com>
2024-08-29 22:03:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css'
+import logo from './logo.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='Cont'>
+      <div className='side'>
+        <div className='navi'>
+        <ul>
+        <img src={logo}></img>
+          <li></li>
+        <li><a>Student Management</a></li>
+        <li><a>Finanial Management</a></li>
+        <li><a>Query Control</a></li>
+        <li><a>Attendance</a></li>
+
+        </ul>
+        </div>
+      </div>
+
+      <div className='main'>
+        <div className='mavi'>
+        <div className='head'>
+          <h1 className='gg'>Student Management</h1>
+        </div>
+        <div className='stu'>
+        <a>Register Enrollment</a>
+          <a>Remedial Enrollment</a>
+          <a>In Paid Club</a>
+        </div>
+        <h1>Menu</h1>
+        <div className='menu'>
+          
+
+          <div className='gri'>
+          <a>Register Enrollment</a>
+          <a>Remedial Enrollment</a>
+          <a>Club Management</a>
+          <a>Classroom Management</a>
+          <a>SMS / EMAIL</a>
+          <a>Attachment</a>
+          </div>
+        </div>
+      </div>
+      </div>
+
+    </div>
     </div>
   );
 }
